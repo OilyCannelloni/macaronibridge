617f0eae40c5d87a5ab3f4011a9d754eaaaf58a1
Krystyna Gasinska <krystyna.gas01@gmail.com>
2024-01-22 20:48:18 +0100

    script fix

--- exportlib.sh
+++ exportlib.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TARGET_DIR="$HOME/.macaronibridge"
 
 if [ ! -d "$TARGET_DIR" ]; then
     mkdir "$TARGET_DIR"
 fi
 
-cp -r ./lib/*.sty "$TARGET_DIR"
+cp -r "$SCRIPT_DIR/lib/"*.sty "$TARGET_DIR"
 
 export TEXINPUTS="$TARGET_DIR//:$TEXINPUTS"
---
+++ lib/REAMDE.md
@@ -0,0 +1,4 @@
+Remember to export the library after any change:
+```
+../exportlib.sh
+```
